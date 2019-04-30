9da046495a240fa5395a6bb4c74333a8e0703c3c
Saint-Jean Germila <germila@noukod.com>
2019-04-30 15:14:59 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -78,4 +78,4 @@ const mapDispatchToProps = dispatch => {
 
 
 //connect this component by wrapping RestaurantInput below
-export default connect ( null, {addRestaurant}) RestaurantInput
+export default connect ( null, {addRestaurant}) (RestaurantInput)
